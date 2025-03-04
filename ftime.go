c1bf798b41c7659faeecdc86e238f8664c909f4e
Tai Kedzierski <dch.tai@gmail.com>
2025-03-04 11:45:01 +0000

    Working example

--- ftime.go
+++ ftime.go
@@ -3,7 +3,7 @@
  * Linux `time` command frequently writes strange output formatting on console if attempting to store
  * the result, and it's sometimes also hard to dissociate it from the program outputs.
  *
- * This simple program runs a command, continually prints its output, and then writes the duration to a file.
+ * This simple program runs a command, dumping output, and then writes the duration to a specified file.
  */
 
 package main
@@ -13,76 +13,78 @@ import (
     "time"
     "os/exec"
     "os"
-    "bufio"
     "log"
     "strings"
-    "io"
+    "path/filepath"
 )
+
+const ERR_UNKNOWN = 120
 
 func main() {
     if len(os.Args) < 3 {
-        fmt.Printf("%s TIMERFILE COMMAND ...\n", os.Args[0])
+        binname := filepath.Base(os.Args[0])
+        fmt.Printf("%s TIMERFILE COMMAND ...\n", binname)
+        fmt.Printf("e.g.\n")
+        fmt.Printf("    %s t.times find ~/ -name '*.mp3'\n", binname)
         os.Exit(1)
     }
 
+    // Pre-emptively create file to be sure we can actually access it
+    //  (at least at ths start...)
+    f := openFile(os.Args[1])
+    f.Close()
+
     t0 := time.Now()
-
     status := runCommand(os.Args[2], os.Args[3:]...)
-
     t1 := time.Now()
+
     writeTime(os.Args[1], t1.Sub(t0), strings.Join(os.Args[2:], " "))
 
     os.Exit(status)
 }
 
-func writeTime(filename string, tdiff time.Duration, command string) {
+func openFile(filename string) *os.File {
     f, err := os.OpenFile(filename, os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
     if err != nil {
-        log.Fatal("Could not open timings.txt file")
+        log.Fatal(fmt.Sprintf("Could not open '%s' file", filename))
     }
+
+    return f
+}
+
+func writeTime(filename string, tdiff time.Duration, command string) {
+    f := openFile(filename)
     defer f.Close()
 
     message := fmt.Sprintf("[%v] : %s\n", tdiff, command)
 
-    if _, err = f.WriteString(message); err != nil {
+    if _, err := f.WriteString(message); err != nil {
         log.Fatal("Could not write timing")
     }
 }
 
-/* Simulat a non-capturing shell command execution
- */
 func runCommand(command string, args... string) int {
     cmd := exec.Command(command, args...)
-    stdout, err := cmd.StdoutPipe()
+
+    // https://blog.kowalczyk.info/article/wOYk/advanced-command-execution-in-go-with-osexec.html
+    cmd.Stdout = os.Stdout
+    cmd.Stderr = os.Stderr
+
+    // We need to use Start() if we want to later use Wait() and get the exit status information
+    //   else we could have just used Run()
+    err := cmd.Start()
     if err != nil {
         log.Fatal(err)
     }
-    cmd.Start()
 
-    reader := bufio.NewReader(stdout)
-    buffer := make([]byte, 1024)
-    for {
-        /* FIXME - this Read(buf) will attempt to wait for a full buffer's worth before returning
-         * Ideally, should return whatever's currently available (e.g. if subprocessed is momentarily not outputting)
-         */
-        count, readerr := reader.Read(buffer)
-        if readerr != nil && readerr != io.EOF {
-            fmt.Printf("! %v\n", readerr)
-            log.Fatal("Process output reading failed")
-        }
-
-        if count == 0 {
-            if err := cmd.Wait(); err != nil {
-                // Linux-only. Unsure what this does on Windows
-                if exiterr, ok := err.(*exec.ExitError); ok {
-                    return exiterr.ExitCode()
-                } else {
-                    return 1
-                }
-            }
-            return 0
+    // https://stackoverflow.com/a/10385867/2703818
+    if err = cmd.Wait(); err != nil {
+        if exiterr, ok := err.(*exec.ExitError); ok {
+            return exiterr.ExitCode()
         } else {
-            fmt.Printf("%s", buffer[:count])
+            return ERR_UNKNOWN
         }
     }
+    return 0
 }
+
